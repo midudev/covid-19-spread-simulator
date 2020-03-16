7273b9447238f37041f4c6c1af65c6a10b5549d3
midudev <miduga@gmail.com>
2020-03-16 15:53:09 +0100

    First commit

---
+++ src/dom.js
@@ -0,0 +1,7 @@
+const $ = id => document.getElementById(id)
+
+export const replayButton = $('replay')
+export const deathFilter = $('deaths')
+export const stayHomeFilter = $('stay-home')
+export const graphElement = $('graph')
+export const replayElement = $('replay')
