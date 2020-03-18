0f7731934c2bea38e3693c2bc8e19ea455e70d57
zaxwebs <zifidigital@gmail.com>
2020-03-18 21:47:31 +0530

    Added max concurrent sick counter

--- src/Ball.js
+++ src/Ball.js
@@ -1,104 +1,119 @@
-import { BALL_RADIUS, COLORS, MORTALITY_PERCENTATGE, TICKS_TO_RECOVER, RUN, SPEED, STATES } from './options.js'
+import {
+	BALL_RADIUS,
+	COLORS,
+	MORTALITY_PERCENTATGE,
+	TICKS_TO_RECOVER,
+	RUN,
+	SPEED,
+	STATES
+} from './options.js'
 import { checkCollision, calculateChangeDirection } from './collisions.js'
 
 const diameter = BALL_RADIUS * 2
 
 export class Ball {
-  constructor ({ x, y, id, state, sketch, hasMovement }) {
-    this.x = x
-    this.y = y
-    this.vx = sketch.random(-1, 1) * SPEED
-    this.vy = sketch.random(-1, 1) * SPEED
-    this.sketch = sketch
-    this.id = id
-    this.state = state
-    this.timeInfected = 0
-    this.hasMovement = hasMovement
-    this.hasCollision = true
-    this.survivor = false
-  }
+	constructor({ x, y, id, state, sketch, hasMovement }) {
+		this.x = x
+		this.y = y
+		this.vx = sketch.random(-1, 1) * SPEED
+		this.vy = sketch.random(-1, 1) * SPEED
+		this.sketch = sketch
+		this.id = id
+		this.state = state
+		this.timeInfected = 0
+		this.hasMovement = hasMovement
+		this.hasCollision = true
+		this.survivor = false
+	}
 
-  checkState () {
-    if (this.state === STATES.infected) {
-      if (RUN.filters.death && !this.survivor && this.timeInfected >= TICKS_TO_RECOVER / 2) {
-        this.survivor = this.sketch.random(100) >= MORTALITY_PERCENTATGE
-        if (!this.survivor) {
-          this.hasMovement = false
-          this.state = STATES.death
-          RUN.results[STATES.infected]--
-          RUN.results[STATES.death]++
-          return
-        }
-      }
+	checkState() {
+		if (this.state === STATES.infected) {
+			if (
+				RUN.filters.death &&
+				!this.survivor &&
+				this.timeInfected >= TICKS_TO_RECOVER / 2
+			) {
+				this.survivor = this.sketch.random(100) >= MORTALITY_PERCENTATGE
+				if (!this.survivor) {
+					this.hasMovement = false
+					this.state = STATES.death
+					RUN.results[STATES.infected]--
+					RUN.results[STATES.death]++
+					return
+				}
+			}
 
-      if (this.timeInfected >= TICKS_TO_RECOVER) {
-        this.state = STATES.recovered
-        RUN.results[STATES.infected]--
-        RUN.results[STATES.recovered]++
-      } else {
-        this.timeInfected++
-      }
-    }
-  }
+			if (this.timeInfected >= TICKS_TO_RECOVER) {
+				this.state = STATES.recovered
+				RUN.results[STATES.infected]--
+				RUN.results[STATES.recovered]++
+			} else {
+				this.timeInfected++
+			}
+		}
+	}
 
-  checkCollisions ({ others }) {
-    if (this.state === STATES.death) return
+	checkCollisions({ others }) {
+		if (this.state === STATES.death) return
 
-    for (let i = this.id + 1; i < others.length; i++) {
-      const otherBall = others[i]
-      const { state, x, y } = otherBall
-      if (state === STATES.death) continue
+		for (let i = this.id + 1; i < others.length; i++) {
+			const otherBall = others[i]
+			const { state, x, y } = otherBall
+			if (state === STATES.death) continue
 
-      const dx = x - this.x
-      const dy = y - this.y
+			const dx = x - this.x
+			const dy = y - this.y
 
-      if (checkCollision({ dx, dy, diameter: BALL_RADIUS * 2 })) {
-        const { ax, ay } = calculateChangeDirection({ dx, dy })
+			if (checkCollision({ dx, dy, diameter: BALL_RADIUS * 2 })) {
+				const { ax, ay } = calculateChangeDirection({ dx, dy })
 
-        this.vx -= ax
-        this.vy -= ay
-        otherBall.vx = ax
-        otherBall.vy = ay
+				this.vx -= ax
+				this.vy -= ay
+				otherBall.vx = ax
+				otherBall.vy = ay
 
-        // both has same state, so nothing to do
-        if (this.state === state) return
-        // if any is recovered, then nothing happens
-        if (this.state === STATES.recovered || state === STATES.recovered) return
-        // then, if some is infected, then we make both infected
-        if (this.state === STATES.infected || state === STATES.infected) {
-          this.state = otherBall.state = STATES.infected
-          RUN.results[STATES.infected]++
-          RUN.results[STATES.well]--
-        }
-      }
-    }
-  }
+				// both has same state, so nothing to do
+				if (this.state === state) return
+				// if any is recovered, then nothing happens
+				if (this.state === STATES.recovered || state === STATES.recovered)
+					return
+				// then, if some is infected, then we make both infected
+				if (this.state === STATES.infected || state === STATES.infected) {
+					this.state = otherBall.state = STATES.infected
+					RUN.results[STATES.infected]++
+					RUN.results[STATES.well]--
+				}
+			}
+		}
+	}
 
-  move () {
-    if (!this.hasMovement) return
+	move() {
+		if (!this.hasMovement) return
 
-    this.x += this.vx
-    this.y += this.vy
+		this.x += this.vx
+		this.y += this.vy
 
-    // check horizontal walls
-    if (
-      (this.x + BALL_RADIUS > this.sketch.width && this.vx > 0) ||
-      (this.x - BALL_RADIUS < 0 && this.vx < 0)) {
-      this.vx *= -1
-    }
+		// check horizontal walls
+		if (
+			(this.x + BALL_RADIUS > this.sketch.width && this.vx > 0) ||
+			(this.x - BALL_RADIUS < 0 && this.vx < 0)
+		) {
+			this.vx *= -1
+		}
 
-    // check vertical walls
-    if (
-      (this.y + BALL_RADIUS > this.sketch.height && this.vy > 0) ||
-      (this.y - BALL_RADIUS < 0 && this.vy < 0)) {
-      this.vy *= -1
-    }
-  }
+		// check vertical walls
+		if (
+			(this.y + BALL_RADIUS > this.sketch.height && this.vy > 0) ||
+			(this.y - BALL_RADIUS < 0 && this.vy < 0)
+		) {
+			this.vy *= -1
+		}
+	}
 
-  render () {
-    const color = COLORS[this.state]
-    this.sketch.noStroke()
-    this.sketch.fill(color)
-    this.sketch.ellipse(this.x, this.y, diameter, diameter)
-  }
+	render() {
+		const color = COLORS[this.state]
+		this.sketch.noStroke()
+		this.sketch.fill(color)
+		this.sketch.ellipse(this.x, this.y, diameter, diameter)
+	}
 }
