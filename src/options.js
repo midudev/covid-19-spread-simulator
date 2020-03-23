9b9920e9793d5f5ac9306d442ccbd6187704ac96
BaconBoi95 <baconboi95@icloud.com>
2020-03-23 19:32:31 -0400

    Update options.js

--- src/options.js
+++ src/options.js
@@ -3,37 +3,37 @@ DEFAULT_FILTERS = {
   stayHome: false
 }
 
-export CANVAS_SIZE = {
+export const CANVAS_SIZE = {
   height: 880,
   width: 360
 }
 
-export DESKTOP_CANVAS_SIZE = {
+export const DESKTOP_CANVAS_SIZE = {
   height: 400,
   width: 800
 }
 
-export BALL_RADIUS = 5
-export COLORS = {
+export const BALL_RADIUS = 5
+export const COLORS = {
   death: '#c50000',
   recovered: '#D88DBC',
   infected: '#5ABA4A',
   well: '#63C8F2'
 }
 
-export STATES = {
+export const STATES = {
   infected: 'infected',
   well: 'well',
   recovered: 'recovered',
   death: 'death'
 }
 
-export COUNTERS = {
+export const COUNTERS = {
   ...STATES,
   'max-concurrent-infected': 'max-concurrent-infected'
 }
 
-export STARTING_BALLS = {
+export const STARTING_BALLS = {
   [STATES.infected]: 1,
   [STATES.well]: 199,
   [STATES.recovered]: 0,
@@ -41,19 +41,19 @@ export STARTING_BALLS = {
   'max-concurrent-infected': 0
 }
 
-export RUN = {
+export const RUN = {
   filters: { ...DEFAULT_FILTERS },
   results: { ...STARTING_BALLS },
   tick: 0
 }
 
-export MORTALITY_PERCENTATGE = 5
-export SPEED = 1
-export TOTAL_TICKS = 1600
-export TICKS_TO_RECOVER = 500
-export STATIC_PEOPLE_PERCENTATGE = 25
+export const MORTALITY_PERCENTATGE = 5
+export const SPEED = 1
+export const TOTAL_TICKS = 1600
+export const TICKS_TO_RECOVER = 500
+export const STATIC_PEOPLE_PERCENTATGE = 25
 
-export resetRun = () => {
+export const resetRun = () => {
   RUN.results = { ...STARTING_BALLS }
   RUN.tick = 0
 }
